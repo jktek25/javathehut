c0ae281f4265341555290930f2d797fdd45b30ac
JLouie <jlouie25@outlook.com>
2026-08-24 21:58:39 -0600

    Extend calculator and tests for more functionality
    - Add multiply
    - Add divide
    - Add recursiveMultiply
    - Add recursiveDiv
    - Add isOdd
    - Add isEven
    - Add square
    - Add factorial
    - Add tests

--- src/main/java/com/jktek25/app/Calculator.java
+++ src/main/java/com/jktek25/app/Calculator.java
@@ -32,13 +32,72 @@ public class Calculator {
     }
 
     /**
-     * Recursively adds two integers.
+     * Multiplies two integers.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @return the product of the two integers
+     */
+    public int multiply(final int num1, final int num2) {
+        return (num1 * num2);
+    }
+
+    /**
+     * Divides two integers.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @return the quotient of the two integers
+     */
+    public int divide(final int num1, final int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return (num1 / num2);
+    }
+
+    /**
+     * Calculates the square of an integer.
+     *
+     * @param num the integer to square
+     * @return the square of the integer
+     */
+    public int square(final int num) {
+        return (num * num);
+    }
+
+    /**
+     * Calculates the factorial of an integer.
+     *
+     * @param num the integer to calculate factorial for
+     * @return the factorial of the integer
+     */
+    public int factorial(final int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException(
+                "Factorial is not defined for negative numbers.");
+        }
+        if (num == 0 || num == 1) {
+            return 1;
+        }
+        return num * this.factorial(num - 1);
+    }
+
+    /**
+     * Recursively adds two integers. Maximum allowed for num2 is 5000 to
+     * prevent stack overflow.
      *
      * @param num1 the first integer
      * @param num2 the second integer
      * @return the sum of the two integers
      */
     public int recursiveAdd(final int num1, final int num2) {
+        final int maxLimit = 5000;
+        if (num2 > maxLimit) {
+            throw new IllegalArgumentException(
+                "num2 is too large for recursion. Maximum allowed is "
+                + maxLimit + ".");
+        }
         if (num2 == 0) {
             return num1;
         } else if (num2 > 0) {
@@ -51,13 +110,26 @@ public class Calculator {
     }
 
     /**
-     * Recursively subtracts two integers.
+     * Recursively subtracts two integers. Maximum allowed for num2 is 5000 to
+     * prevent stack overflow. Minimum allowed for num2 is -5000 to prevent
+     * stack overflow.
      *
      * @param num1 the first integer
      * @param num2 the second integer
      * @return the difference of the two integers
      */
     public int recursiveSub(final int num1, final int num2) {
+        final int minLimit = -5000;
+        final int maxLimit = 5000;
+        if (num2 < minLimit) {
+            throw new IllegalArgumentException(
+                "num2 is too small for recursion. Minimum allowed is "
+                + minLimit + ".");
+        } else if (num2 > maxLimit) {
+            throw new IllegalArgumentException(
+                "num2 is too large for recursion. Maximum allowed is "
+                + maxLimit + ".");
+        }
         if (num2 == 0) {
             return num1;
         } else if (num2 > 0) {
@@ -69,4 +141,85 @@ public class Calculator {
         }
     }
 
+    /**
+     * Recursively multiplies two integers.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @return the product of the two integers
+     */
+    public int recursiveMultiply(final int num1, final int num2) {
+        if (num2 == 0) {
+            return 0;
+        } else if (num2 > 0) {
+            return num1 + this.recursiveMultiply(num1, num2 - 1);
+        } else {
+            return -this.recursiveMultiply(num1, -num2);
+        }
+    }
+
+    /**
+     * Recursively divides two integers.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @return the quotient of the two integers
+     */
+    public int recursiveDiv(final int num1, final int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        if (num1 < num2) {
+            return 0;
+        } else {
+            return 1 + this.recursiveDiv(num1 - num2, num2);
+        }
+    }
+
+    /**
+     * Checks if a number is even.
+     *
+     * @param num the integer to check
+     * @return true if the number is even, false otherwise
+     */
+    public boolean isEven(final int num) {
+        return (num % 2 == 0);
+    }
+
+    /**
+     * Checks if a number is odd.
+     *
+     * @param num the integer to check
+     * @return true if the number is odd, false otherwise
+     */
+    public boolean isOdd(final int num) {
+        return (num % 2 != 0);
+    }
+
+    /**
+     * Main method to demonstrate the functionality of the Calculator class.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        final Calculator calculator = new Calculator();
+        final int num1 = 5;
+        final int num2 = 3;
+        System.out.println("Add: " + calculator.add(num1, num2));
+        System.out.println("Subtract: " + calculator.sub(num1, num2));
+        System.out.println("Multiply: " + calculator.multiply(num1, num2));
+        System.out.println("Divide: " + calculator.divide(num1, num2));
+        System.out.println("Square: " + calculator.square(num1));
+        System.out.println("Factorial: " + calculator.factorial(num1));
+        System.out.println("Recursive Add: "
+            + calculator.recursiveAdd(num1, num2));
+        System.out.println("Recursive Subtract: "
+            + calculator.recursiveSub(num1, num2));
+        System.out.println("Recursive Multiply: "
+            + calculator.recursiveMultiply(num1, num2));
+        System.out.println("Recursive Divide: "
+            + calculator.recursiveDiv(num1, num2));
+        System.out.println("Is Even: " + calculator.isEven(num1));
+        System.out.println("Is Odd: " + calculator.isOdd(num1));
+    }
 }
--- src/test/java/com/jktek25/app/CalculatorTests.java
+++ src/test/java/com/jktek25/app/CalculatorTests.java
@@ -66,6 +66,170 @@ public class CalculatorTests {
     }
 
     /**
+     * Test the multiply method of the Calculator class with various inputs and
+     * expected results.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @param exp the expected result of multiplying num1 and num2
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, 0, 0",
+        "3, 2, 6",
+        "5, 3, 15",
+        "10, 10, 100",
+        "-1, -2, 2",
+        "5, 5, 25",
+        "0, -1000, 0",
+        "2147483647, -1, -2147483647",
+        "-2147483648, 1, -2147483648"
+    })
+    final void multiplyTest(final int num1, final int num2, final int exp) {
+        Calculator oCalc = new Calculator();
+        int retVal = oCalc.multiply(num1, num2);
+        assertEquals(exp, retVal);
+    }
+
+    /**
+     * Test the divide method of the Calculator class with various inputs and
+     * expected results.
+     *
+     * @param num1 the first integer
+     * @param num2 the second integer
+     * @param exp the expected result of dividing num1 by num2
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1, 0",
+        "3, 1, 3",
+        "500, 2, 250",
+        "10, 10, 1",
+        "20, 10, 2",
+        "55555, 5, 11111",
+        "0, -1, 0",
+        "2147483647, -1, -2147483647",
+        "-2147483648, 1, -2147483648"
+    })
+    final void divideTest(final int num1, final int num2, final int exp) {
+        Calculator oCalc = new Calculator();
+        int retVal = oCalc.divide(num1, num2);
+        assertEquals(exp, retVal);
+    }
+
+    /**
+     * Test the square method of the Calculator class with various inputs and
+     * expected results.
+     *
+     * @param num the integer to be squared
+     * @param exp the expected result of squaring num
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, 0",
+        "1, 1",
+        "2, 4",
+        "3, 9",
+        "5, 25",
+        "4, 16",
+        "8, 64",
+        "16, 256",
+        "32, 1024",
+        "64, 4096",
+        "128, 16384",
+        "10, 100",
+        "-1, 1",
+        "0, 0",
+        "1000, 1000000"
+    })
+    final void squareTest(final int num, final int exp) {
+        Calculator oCalc = new Calculator();
+        int retVal = oCalc.square(num);
+        assertEquals(exp, retVal);
+    }
+
+    /**
+     * Test the factorial method of the Calculator class with various inputs
+     * and expected results.
+     *
+     * @param num the integer to calculate the factorial of
+     * @param exp the expected result of calculating the factorial of num
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1",
+        "1, 1",
+        "2, 2",
+        "3, 6",
+        "4, 24",
+        "5, 120",
+        "6, 720",
+        "7, 5040",
+        "8, 40320",
+        "9, 362880",
+        "10, 3628800"
+    })
+    final void factorialTest(final int num, final int exp) {
+        Calculator oCalc = new Calculator();
+        int retVal = oCalc.factorial(num);
+        assertEquals(exp, retVal);
+    }
+
+    /**
+     * Test isEven method of the Calculator class with various inputs and
+     * expected results.
+     *
+     * @param num the integer to check
+     * @param exp the expected result of checking if num is even
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, true",
+        "1, false",
+        "2, true",
+        "3, false",
+        "4, true",
+        "5, false",
+        "6, true",
+        "7, false",
+        "8, true",
+        "9, false",
+        "10, true"
+    })
+    final void isEvenTest(final int num, final boolean exp) {
+        Calculator oCalc = new Calculator();
+        boolean retVal = oCalc.isEven(num);
+        assertEquals(exp, retVal);
+    }
+
+    /**
+     * Test isOdd method of the Calculator class with various inputs and
+     * expected results.
+     *
+     * @param num the integer to check
+     * @param exp the expected result of checking if num is odd
+     */
+    @ParameterizedTest
+    @CsvSource({
+        "0, false",
+        "1, true",
+        "2, false",
+        "3, true",
+        "4, false",
+        "5, true",
+        "6, false",
+        "7, true",
+        "8, false",
+        "9, true",
+        "10, false"
+    })
+    final void isOddTest(final int num, final boolean exp) {
+        Calculator oCalc = new Calculator();
+        boolean retVal = oCalc.isOdd(num);
+        assertEquals(exp, retVal);
+    }
+
+    /**
      * Test the recursiveAdd method of the Calculator class with various
      * inputs and expected results.
      *
@@ -90,7 +254,7 @@ public class CalculatorTests {
         assertEquals(exp, retVal);
     }
 
-    /**
+     /**
      * Test the recursiveSub method of the Calculator class with various inputs
      * and expected results.
      *
@@ -109,7 +273,7 @@ public class CalculatorTests {
         "1000, 1000, 0", // Keep it small for recursion
         "5000, 5000, 0"
     })
-    final void recursiveSubTest(final int num1, final int num2, final int exp) {
+   final void recursiveSubTest(final int num1, final int num2, final int exp) {
         Calculator oCalc = new Calculator();
         int retVal = oCalc.recursiveSub(num1, num2);
         assertEquals(exp, retVal);
